cf0811d523a799f267e8d39c57a355a3975c6c2c
pavel <pavel@comp1.(none)>
2013-07-17 16:30:05 +1100

    version 0.0

--- public/index.js
+++ public/index.js
@@ -6,14 +6,14 @@ function xmlStart() {
 <!-- yml по каждому товару(запускается на каждой странице) -->
 function xmlOffers() {
     for (i = 0; i < json.itemList.length; i++) {
-        var xml = '<offer id="' + json.itemList[i].itemId + '" available="true"><url>' + json.itemList[i].href + '</url><price>'+ json.itemList[i].price +' руб.</price><currencyId>RUR</currencyId><picture>' + json.itemList[i].image + '</picture><name>Товар #'+ i +' </name></offer>';
+        var xml = '<offer id="' + json.itemList[i].itemId + '" type="vendor.model" available="true"><name>Товар #'+ i +' </name><url>' + json.itemList[i].href + '</url><price>'+ json.itemList[i].price +' руб.</price><currencyId>RUR</currencyId><categoryId>1</categoryId><picture>' + json.itemList[i].image + '</picture><picture>' + json.itemList[i].image + '</picture><vendor>TaoBao</vendor><param name="images"><image id="'+i+'">' + json.itemList[i].image + '</image></param><param name="Цвет"><color id="1">Синий</color><color id="2">Зеленый</color></param><param name="Размер"><size id="1">XXL</size><size id="2">LLX</size></param></offer>';
         $("#yml")[0].value += xml;
     }
 	$('#parse').attr('disabled','disabled');
 }
 
 <!-- Конец yml -->
-function xmlEnd() { $("#yml")[0].value = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE yml_catalog SYSTEM "shops.dtd"><yml_catalog date="2010-04-01 17:00"><shop><name>BestShop</name><company>Best online seller Inc.</company><url>http://best.seller.ru/</url><platform>CMS</platform><version>2.3</version><agency>Agency</agency><email>CMS@CMS.ru</email><currencies><currency id="RUR" rate="1"/></currencies><categories><category id="1">Одежда</category></categories><offers>' + $("#yml")[0].value + '</offers></shop></yml_catalog>';
+function xmlEnd() { $("#yml")[0].value = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE yml_catalog SYSTEM "shops.dtd"><yml_catalog date="2013-04-01 17:00"><shop><name>BestShop</name><company>Best online seller Inc.</company><url>http://best.seller.ru/</url><platform>CMS</platform><version>1.0</version><agency>Agency</agency><email>CMS@CMS.ru</email><currencies><currency id="RUR" rate="1"/></currencies><categories><category id="1">Категория #1</category></categories><offers>' + $("#yml")[0].value + '</offers></shop></yml_catalog>';
 $("#yml")[0].style.display = 'block';
 $("#parse")[0].style.display = 'none';
 $("#end")[0].style.display = 'none'; 
