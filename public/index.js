c0e37a3fddb5cd4ea0d6f574063f67703594547f
pavel <pavel@comp1.(none)>
2013-07-19 12:00:00 +1100

    version 0.1

--- public/index.js
+++ public/index.js
@@ -1,22 +1,30 @@
 <!-- Форма + кнопки -->
 function xmlStart() {
-    $("body").prepend('<textarea id="yml" style="width:100%; height:500px; display:none;" /><input id="parse" type="button" value="Парсить страницу" onClick="javascript: xmlOffers()" /><input id="end" type="button" value="Готово" onClick="javascript: xmlEnd()" />');
+    $("body").prepend('<div class="ymlYandex" style="display:none;">Yandex yml</div><textarea id="ymlYandex" style="width:100%; height:500px; display:none;" /><div class="ymlMail" style="padding-top:20px; display:none;">Mail yml</div><textarea id="ymlMail" style="width:100%; height:500px; display:none;" /><input id="parse" type="button" value="Парсить страницу" onClick="javascript: xmlOffer()" /><input id="end" type="button" value="Готово" onClick="javascript: xmlEndYandex(); xmlEndMail()" />'/*<input type="text" id="kat"/><input id="end" type="button" value="Добавить категорию" onClick="javascript: addKat()" />*/);
 }
 
 <!-- yml по каждому товару(запускается на каждой странице) -->
-function xmlOffers() {
+function xmlOffer() {
     for (i = 0; i < json.itemList.length; i++) {
-        var xml = '<offer id="' + json.itemList[i].itemId + '" type="vendor.model" available="true"><name>Товар #'+ i +' </name><url>' + json.itemList[i].href + '</url><price>'+ json.itemList[i].price +' руб.</price><currencyId>RUR</currencyId><categoryId>1</categoryId><picture>' + json.itemList[i].image + '</picture><picture>' + json.itemList[i].image + '</picture><vendor>TaoBao</vendor><param name="images"><image id="'+i+'">' + json.itemList[i].image + '</image></param><param name="Цвет"><color id="1">Синий</color><color id="2">Зеленый</color></param><param name="Размер"><size id="1">XXL</size><size id="2">LLX</size></param></offer>';
-        $("#yml")[0].value += xml;
+        var xml = '<offer id="' + json.itemList[i].itemId + '" available="true"><url>' + json.itemList[i].href + '</url><price>'+ json.itemList[i].price +' руб.</price><currencyId>RUR</currencyId><categoryId>1</categoryId><picture>' + json.itemList[i].image + '</picture><picture>' + json.itemList[i].image + '</picture><name>Товар #'+ i +' </name><param name="Цвет">Синий,Зеленый,Красный</param><param name="Размер">XXL,LLX</param></offer>';
+        $("#ymlYandex")[0].value += xml;
+	$("#ymlMail")[0].value += xml;
     }
 	$('#parse').attr('disabled','disabled');
 }
 
-<!-- Конец yml -->
-function xmlEnd() { $("#yml")[0].value = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE yml_catalog SYSTEM "shops.dtd"><yml_catalog date="2013-04-01 17:00"><shop><name>BestShop</name><company>Best online seller Inc.</company><url>http://best.seller.ru/</url><platform>CMS</platform><version>1.0</version><agency>Agency</agency><email>CMS@CMS.ru</email><currencies><currency id="RUR" rate="1"/></currencies><categories><category id="1">Категория #1</category></categories><offers>' + $("#yml")[0].value + '</offers></shop></yml_catalog>';
-$("#yml")[0].style.display = 'block';
+<!-- Начало + конец ymlYandex -->
+function xmlEndYandex() { $("#ymlYandex")[0].value = '<?xml version="1.0" encoding="utf-8"?><!DOCTYPE yml_catalog SYSTEM "shops.dtd"><yml_catalog date="2013-04-01 17:00"><shop><name>BestShop</name><company>Best online seller Inc.</company><url>http://best.seller.ru/</url><currencies><currency id="RUR" rate="1"/><currency id="USD" rate="32" /></currencies><categories><category id="1">Категория #1</category></categories><offers>' + $("#ymlYandex")[0].value + '</offers></shop></yml_catalog>';
+$("#ymlYandex")[0].style.display = 'block';
+$(".ymlYandex")[0].style.display = 'block';
 $("#parse")[0].style.display = 'none';
 $("#end")[0].style.display = 'none'; 
+}
+
+<!-- Начало + конец ymlMail -->
+function xmlEndMail() { $("#ymlMail")[0].value = '<torg_price date="2013-04-01 17:00"><shop><shopname>BestShop</shopname><company>Best online seller Inc.</company><url>http://best.seller.ru/</url><platform>CMS</platform><version>1.0</version><agency>Agency</agency><email>CMS@CMS.ru</email><currencies><currency id="RUR" rate="1"/></currencies><categories><category id="1">Категория #1</category></categories><offers>' + $("#ymlMail")[0].value + '</offers></shop></torg_price>';
+$("#ymlMail")[0].style.display = 'block';
+$(".ymlMail")[0].style.display = 'block';
 }
 
 $('li.top-item, li.sub-low-item, .paginator a:not(.active)').click(function() {
